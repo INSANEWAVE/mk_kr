fc0df6b3237aa0ba3c909c7f4cc87279131fe059
INSANEWAVE <alxbelter@outlook.com>
2025-01-17 15:03:25 +0300

    Полная переработка кода. Сейчас выводит counter

--- src/src.ino
+++ src/src.ino
@@ -5,7 +5,8 @@
 #define clock 13 
 #define data 12 
 #define latch 10
-#define sensorPin A0 // Пин фоторезистора
+#define sensorPin A5 // Пин фоторезистора
+
 
 Servo myservo;
 Servo motor;
@@ -16,11 +17,13 @@ unsigned long next_time_3;
 int timeout = 5;        // Частота обновления датчика оборотов
 int timeout2 = 1000;   // Частота вычисления оборотов в секунду
 int timeout3 = 500;   // Частота обновления таймера работы электромотора
-int sensorValue = 0;   // Переменная для хранения значения с фоторезистора
-int counter = 0; //Счётчик оборотов
+int sensorValue = 0; // Переменная для хранения значения с фоторезистора
+bool elementDetected = false;
+   
+long counter = 0;      //Счётчик оборотов
 void setup() {
-  motor.attach(6,1000,2000);
-  myservo.attach(7);
+  motor.attach(9);  //Подключение мотора
+  myservo.attach(7);          //Подключение сервопривода
   myservo.write(180);
   delay(1000);
   myservo.write(0);
@@ -31,7 +34,7 @@ void setup() {
   pinMode(data, OUTPUT);
   pinMode(latch, OUTPUT);
   digitalWrite(latch, HIGH); //Это надо заменить на подачу питания с шины 5 вольт по возможности
-  Serial.begin(9600);   // Инициализация Serial для вывода данных.
+  Serial.begin(250000);   // Инициализация Serial для вывода данных.
   next_time = millis() + timeout;
   next_time_2 = millis() + timeout2;
   next_time_3 = millis() + timeout3; // вычисляем время следующего переключения
@@ -39,29 +42,40 @@ void setup() {
 
 
 void loop() {
-  int now_time = millis();                          
-  sensorValue = analogRead(sensorPin);            
-  float voltage = sensorValue * (5.0 / 1023.0);     //Конвертируем входной сигнал в вольты
+  unsigned long now_time = millis();                          
+  sensorValue = analogRead(sensorPin);
+  
+       
+  
+          
+  
   
                  
     if( now_time >= next_time ){               
-        next_time = now_time + timeout;  //Условие-счётчик, за выделенное время (timeout2) насчитывает количество высоких сигналов 
-          if(voltage < 4){              //и затем сбрасывается
+      next_time = now_time + timeout;
+      //Serial.println(sensorValue);  //Условие-счётчик, за выделенное время (timeout2) насчитывает количество высоких сигналов 
+      if(sensorValue < 952){
+    elementDetected = true;              //и затем сбрасывается
     counter++;                         // НУЖНО ПЕРЕРАБОТАТЬ ВЕСЬ ТАЙМЕР - НЕТ СЧИТЫВАНИЯ НИЗКИХ ОБОРОТОВ. Теоретический вариант - измерять время между вспышками
-  }       
-         
+  }else {elementDetected = false;}     
+        
          }
+
    if( now_time >= next_time_2 ){               
         next_time_2 = now_time + timeout2;
-          float RPS = counter/(timeout2/1000);
-          float RPM = RPS*60;    
-          Serial.println(RPM);
-          float val = map(RPM, 0, 50, 0, 180);
-          myservo.write(val);                            
+          float RPM = (counter/timeout)*60;
+          Serial.println(counter);   
+          //Serial.println("RPM:");
+          //Serial.println(RPM);
+
+          //float val = map(RPM, 0, 50, 0, 180);
+          //myservo.write(val);                            
           counter = 0;
          } 
    if( now_time >= next_time_3 ){               
-        next_time_3 = now_time + timeout3; //Условие счётчик для управления мотором
+        next_time_3 = now_time + timeout3;
+           
+           
          }
   
 }
