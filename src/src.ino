1c1b64d6e0046fc8890d6d89c435e21ef5790864
INSANEWAVE <alxbelter@outlook.com>
2025-01-19 15:22:37 +0300

    Чистка кода

--- src/src.ino
+++ src/src.ino
@@ -1,15 +1,8 @@
-//#include <GyverEncoder.h> //Библиотека для гибкого управления энкодером
 #include <Servo.h> //Библиотека для работы с сервоприводами и бесколлекторными моторами
-//#include <Shifty.h> //Библиотека для сдвиговых регистров - оптимизация кода
 
-#define clock 13 
-#define data 12 
-#define latch 10
 #define sensorPin A5 // Пин фоторезистора
 
-
 Servo myservo;
-//Servo motor;
 
 unsigned long next_time;
 unsigned long next_time_2;
@@ -22,7 +15,7 @@ bool elementDetected = false;
    
 long counter = 0;      //Счётчик оборотов
 void setup() {
-  //motor.attach(9);  //Подключение мотора
+  
   myservo.attach(7);          //Подключение сервопривода
   myservo.write(0);
   delay(1000);
@@ -30,14 +23,10 @@ void setup() {
   delay(1000);
   myservo.write(0);
   delay(1000);
-  //pinMode(clock, OUTPUT);
-  //pinMode(data, OUTPUT);
-  //pinMode(latch, OUTPUT);
-  //digitalWrite(latch, HIGH); //Это надо заменить на подачу питания с шины 5 вольт по возможности
   Serial.begin(250000);   // Инициализация Serial для вывода данных.
   next_time = millis() + timeout;
   next_time_2 = millis() + timeout2;
-  //next_time_3 = millis() + timeout3; // вычисляем время следующего переключения
+
 }
 
 
@@ -47,9 +36,9 @@ void loop() {
             
     if( now_time >= next_time ){               
       next_time = now_time + timeout;
-      //Serial.println(sensorValue);  //Условие-счётчик, за выделенное время (timeout2) насчитывает количество высоких сигналов 
+      
       if(sensorValue < 940){
-    elementDetected = true;              //и затем сбрасывается
+    elementDetected = true;            
     counter++;                         
   }else {elementDetected = false;}     
         
@@ -66,11 +55,7 @@ void loop() {
           myservo.write(val);                            
           counter = 0;
          } 
-   //if( now_time >= next_time_3 ){               
-   //     next_time_3 = now_time + timeout3;
-   //        
-   //        
-   //      }
+   
   
 }
 
